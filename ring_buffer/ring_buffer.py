721548cf04c73902577066d6f5ebbeaa1c660907
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-02-28 10:21:16 -0800

    Revert "finished mvp and stretch"
    This reverts commit 9e1d1305f64ee6d18b08d4f6bde8b2dec6a60da5.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,25 +8,13 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        if len(self.storage) < self.capacity:
-            self.storage.add_to_tail(item)
-        else:
-            if self.current is self.storage.tail or self.current is None:
-                self.current = self.storage.head
-            else:
-                self.current = self.current.next
-
-            self.current.value = item
+        pass
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-        cur_node = self.storage.head
-        while cur_node:
-            list_buffer_contents.append(cur_node.value)
-            cur_node = cur_node.next
 
         return list_buffer_contents
 
@@ -35,18 +23,10 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        self.cur_item_idx = 0
-        self.storage = [None] * capacity
+        pass
 
     def append(self, item):
-        if None in self.storage:
-            self.storage[self.storage.index(None)] = item
-        else:
-            self.storage[self.cur_item_idx] = item
-            if self.cur_item_idx == len(self.storage) - 1:
-                self.cur_item_idx = 0
-            else:
-                self.cur_item_idx += 1
+        pass
 
     def get(self):
-        return self.storage[0:self.storage.index(None)] if None in self.storage else self.storage
+        pass
